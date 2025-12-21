b600e8e3d623f2be3fb3923d29788d999bcde50f
benjaminsenyo <benjamin.senyo@aims-cameroon.org>
2025-12-21 22:37:03 +0100

    greatest sum function defined

--- PracticeQuestions.py
+++ PracticeQuestions.py
@@ -1,1 +1,8 @@
-print("anma")
+def greatest_sum(array):
+    lists =[]
+    for i in array:
+        if len(array) > 1:
+            maxx = max(array)
+            lists.append(i)
+        sumof = sum(list)
+        print(sumof)
